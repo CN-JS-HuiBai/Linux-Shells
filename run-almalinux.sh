77c52ab9b3c4b96510ee6d05c734dcb8316a102b
CN-JS-HuiBai <h208434317@gmail.com>
2023-12-26 11:12:00 +0800

    modified:   run-almalinux.sh

--- run-almalinux.sh
+++ run-almalinux.sh
@@ -1,8 +1,8 @@
 docker pull almalinux/9-init:latest
 docker run -d \
-    --name almalinux 
-    --privileged=true 
-    docker.io/almalinux/9-init:latest
+    --name almalinux \
+    --privileged=true \
+    docker.io/almalinux/9-init:latest \
     /usr/sbin/init
 
 
