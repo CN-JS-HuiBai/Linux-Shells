9b4d50e6ef64745b45dd2096732677453742803a
CN-JS-HuiBai <huyang@littlediary.cn>
2024-01-07 11:05:30 +0800

    fix some problems

--- rhel-nginx-config.sh
+++ rhel-nginx-config.sh
@@ -2,4 +2,4 @@ dnf -y install nginx nginx*
 mkdir -p /www/nginx.conf.d
 mkdir -p /www/wwwlogs
 mkdir -p /www/nginx.proxy.d
-cp nginx.conf /etc/nginx/nginx.conf
+cp /root/nginx.conf /etc/nginx/nginx.conf
